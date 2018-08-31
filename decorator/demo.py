2be315800e28cea5477453a4d8c075093803a137
wovert <67668283@qq.com>
2018-08-31 09:08:59 +0800

    add wrapper

--- decorator/demo.py
+++ decorator/demo.py
@@ -1,20 +1,44 @@
 import time
 
-def timer(f): # 装饰器函数
-  def inner():    
+'''
+f：被装饰的函数
+'''
+def wrapper(f): # 装饰器函数
+  def inner(*args,**kwargs): # 接受元租
+    '''
+    被装饰函数之前的执行的代码
+    '''
     start = time.time()
-    f() # 被装饰的函数
+
+    f(*args,**kwargs) # 被装饰的函数
+    
+    '''
+    被装饰函数之后的执行的代码
+    '''
     end = time.time()
     print(end - start)
   return inner
 
-@timer # 语法糖，@装饰器函数名
-def func(): # 被装饰的函数
+@wrapper # 语法糖，@装饰器函数名
+def func(a): # 被装饰的函数
   time.sleep(0.01)
-  print('Hi boss~~')
+  print('Hi boss~~', a)
 
 # func = timer(func) 等于  @timer
 # func() = inner()
 #func = timer(func)
-func()
+func(10)
+func(1)
+
+@wrapper # 语法糖，@装饰器函数名
+def func2(a,b): # 被装饰的函数
+  time.sleep(0.01)
+  print('hello father~~', a, b)
+
+# func = timer(func) 等于  @timer
+# func() = inner()
+#func = timer(func)
+func2(10,20)
+
+func2(3,b='bb')
 
